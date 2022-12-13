c5bdd1650beb6a0b3cf1f8c495a117e2589045db
Seu nome <halisterfernando@hotmail.com>
2022-12-13 14:20:25 -0300

    feat: :sparkles: rendezira informacoes do municipio

--- src/components/CountyData.jsx
+++ src/components/CountyData.jsx
@@ -7,7 +7,6 @@ export default function CountyData() {
   const { counties, countyData } = useSelector(allStatesAndCounties);
   const { county } = useSelector(selectedStateAndCounty);
   const { id: countyId } = counties.find(({ nome }) => nome === county);
-  console.log(countyData);
 
   const dispatch = useDispatch();
 
@@ -18,6 +17,38 @@ export default function CountyData() {
   }, [countyId]);
 
   return (
-    <div>CountyData</div>
+    <div>
+      <h2>{`Município: ${county}`}</h2>
+      {
+        countyData.length && (
+          <>
+            <p>
+              {`UF: ${countyData[0].municipio.microrregiao.mesorregiao.UF.nome} - ${countyData[0].municipio.microrregiao.mesorregiao.UF.sigla} `}
+            </p>
+            <p>
+              {`Região: ${countyData[0].municipio.microrregiao.mesorregiao.UF.regiao.nome} - ${countyData[0].municipio.microrregiao.mesorregiao.UF.regiao.sigla}`}
+            </p>
+            <p>
+              {`Microrregião: ${countyData[0].municipio.microrregiao.nome}`}
+            </p>
+            <p>
+              {`Mesorregião: ${countyData[0].municipio.microrregiao.mesorregiao.nome}`}
+            </p>
+          </>
+        )
+      }
+      <h2>Distritos</h2>
+      <ul>
+
+        {
+
+        countyData.length && countyData.map((data) => (
+          // console.log(data.municipio.microrregiao.nome)
+          <li>{data.nome}</li>
+
+        ))
+      }
+      </ul>
+    </div>
   );
 }
