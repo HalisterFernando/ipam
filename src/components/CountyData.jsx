870bea95ae66e51b1e264c125a3d082a3308a89b
Seu nome <halisterfernando@hotmail.com>
2022-12-20 13:51:01 -0300

    feat: :art: renderiza mapa da região do país conforme a região do município

--- src/components/CountyData.jsx
+++ src/components/CountyData.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchCountyData, allStatesAndCounties } from '../redux/features/apiSlice';
 import { selectedStateAndCounty } from '../redux/features/statesCountySlice';
+import Images from '../images/images';
 
 export default function CountyData() {
   const { counties, countyData } = useSelector(allStatesAndCounties);
@@ -18,33 +19,68 @@ export default function CountyData() {
 
   return (
     <div>
-      <h2>{`Município: ${county}`}</h2>
-      {
+      <div className="
+        shadow-sm
+        shadow-ipam-cream
+        rounded
+        mt-4
+        py-4
+        px-2
+        w-11/12
+        mx-auto
+        bg-ipam-l-green
+      "
+      >
+        <h2>
+          <strong>Município:</strong>
+          {' '}
+          {county}
+        </h2>
+
+        {
         countyData.length && (
           <>
             <p>
-              {`UF: ${countyData[0].municipio.microrregiao.mesorregiao.UF.nome} - ${countyData[0].municipio.microrregiao.mesorregiao.UF.sigla} `}
+              <strong>UF:</strong>
+              {' '}
+              {countyData[0].municipio.microrregiao.mesorregiao.UF.nome}
+              {' '}
+              -
+              {' '}
+              {countyData[0].municipio.microrregiao.mesorregiao.UF.sigla}
             </p>
             <p>
-              {`Região: ${countyData[0].municipio.microrregiao.mesorregiao.UF.regiao.nome} - ${countyData[0].municipio.microrregiao.mesorregiao.UF.regiao.sigla}`}
+              <strong>Região:</strong>
+              {' '}
+              {countyData[0].municipio.microrregiao.mesorregiao.UF.regiao.nome}
+              {' '}
+              -
+              {' '}
+              {countyData[0].municipio.microrregiao.mesorregiao.UF.regiao.sigla}
             </p>
             <p>
-              {`Microrregião: ${countyData[0].municipio.microrregiao.nome}`}
+              <strong>Microrregião:</strong>
+              {' '}
+              {countyData[0].municipio.microrregiao.nome}
             </p>
             <p>
-              {`Mesorregião: ${countyData[0].municipio.microrregiao.mesorregiao.nome}`}
+              <strong>Mesorregião:</strong>
+              {' '}
+              {countyData[0].municipio.microrregiao.mesorregiao.nome}
             </p>
           </>
         )
       }
-      <h2>Distritos</h2>
-      <ul>
+        <img className="h-36 drop-shadow-[5px_10px_5px_rgba(0,0,0,0.85)] mx-auto" src={Images[countyData.length ? countyData[0].municipio.microrregiao.mesorregiao.UF.regiao.nome : null]} alt="" />
+      </div>
+      <h1 className="text-center text-xl my-4 font-bold bg-ipam-l-green">Distritos</h1>
+      <ul className="text-center">
 
         {
 
-        countyData.length && countyData.map((data) => (
+        countyData.length && countyData.map(({ nome, id }) => (
           // console.log(data.municipio.microrregiao.nome)
-          <li>{data.nome}</li>
+          <li key={id} className="font-medium">{nome}</li>
 
         ))
       }
---
+++ src/images/images.jsx
@@ -0,0 +1,13 @@
+import Norte from './norte.png';
+import Sul from './sul.png';
+import Nordeste from './nordeste.png';
+import Sudeste from './sudeste.png';
+import CentroOeste from './centro-oeste.png';
+
+export default {
+  Norte,
+  Sul,
+  Nordeste,
+  Sudeste,
+  CentroOeste,
+};
