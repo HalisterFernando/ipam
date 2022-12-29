06feb6da08314dbcc60cf92ed1382a30706d7fff
Seu nome <halisterfernando@hotmail.com>
2022-12-29 14:29:07 -0300

    feat: :sparkles: botão para realizar nova pesquisa

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import { BsArrowLeftCircle } from 'react-icons/bs';
+import { useNavigate } from 'react-router-dom';
 
 export default function Footer() {
+  const isLocationHome = window.location.href.includes('home');
+  const history = useNavigate();
   return (
-    <footer>Footer</footer>
+    <footer className="flex justify-center items-center py-2">
+      {
+        !isLocationHome && (
+        <button
+          type="button"
+          className="flex flex-col items-center text-lg font-semibold"
+          onClick={() => history('/home')}
+        >
+          <i className="text-3xl">
+            {' '}
+            <BsArrowLeftCircle />
+            {' '}
+          </i>
+          Realizar nova pesquisa
+        </button>
+        )
+      }
+    </footer>
   );
 }
